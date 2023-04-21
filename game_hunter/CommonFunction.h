7ff53b2ff94b57c70ad956f3b02d2a79cb49c9d7
cva2019 <caovananhnd2019@gmail.com>
2023-04-21 21:56:41 +0700

    1

---
+++ game_hunter/CommonFunction.h
@@ -0,0 +1,69 @@
+
+
+#ifndef COMMON_FUNCTION_H_
+#define COMMON_FUNCTION_H_
+
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+
+static SDL_Window* g_window = NULL;
+static SDL_Renderer* g_screen = NULL;
+static SDL_Event g_event;
+static Mix_Chunk* g_sound_ex_main=NULL;
+static Mix_Chunk* g_sound_explosion=NULL;
+
+const int FRAMES_PER_SECOND = 30;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 640;
+const int SCREEN_BPP = 32;
+const int SPEED_SCREEN = 2;
+
+const int RENDER_DRAW_COLOR = 0XFF;
+
+const int COLOR_KEY_R = 167;
+const int COLOR_KEY_G = 175;
+const int COLOR_KEY_B = 180;
+
+static char g_name_main_right[]               = {"img//player_right.png"};
+static char g_name_main_left[]              = {"img//player_left.png"};
+
+#define TILE_SIZE 64
+#define BLANK_TILE 0
+
+#define MAX_MAP_X 400
+#define MAX_MAP_Y 10
+
+#define STATE_MONEY 1
+
+
+typedef struct Input
+{
+    int left_;
+    int right_;
+    int up_;
+    int down_;
+    int jump_;
+} Input;
+
+
+typedef struct Map
+{
+    int start_x_;
+    int start_y_;
+    int max_x_;
+    int max_y_;   
+    int tile[MAX_MAP_Y][MAX_MAP_X];
+    char* file_name_;
+} Map;
+
+namespace SDLCommonFunction
+{
+    bool CheckCollision(const SDL_Rect& object1, const SDL_Rect& object2);
+}
+
+#endif
