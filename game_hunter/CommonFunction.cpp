ef3bbcb0ff719c056f9d7d231e9de8924a1fca9b
cva2019 <caovananhnd2019@gmail.com>
2023-04-30 17:45:36 +0700

    3

--- game_hunter/CommonFunction.cpp
+++ game_hunter/CommonFunction.cpp
@@ -2,6 +2,165 @@
 
 #include "CommonFunction.h"
 
+int SDLCommonFunction::ShowMenu(SDL_Renderer* g_screen, TTF_Font* font, 
+                            const std::string& menu1, 
+                            const std::string& menu2,
+                            const std::string& menu3,
+                            const std::string& menu4,
+                            const std::string& img_name)
+{
+    char* ch1 = (char*)menu1.c_str();
+    char* ch2 = (char*)menu2.c_str();
+    char* ch3 = (char*)menu3.c_str();
+    char* ch4 = (char*)menu4.c_str();
+    char* img_file = (char*)img_name.c_str();
+
+    int size1 = menu1.length();
+    int size2 = menu2.length();
+    int size3 = menu3.length();
+    int size4 = menu4.length();
+
+    int time = 0;
+    int x = 0;
+    int y = 0;
+    const int num = 4;
+    char* labels[num];
+
+    labels[0] = new char [size1 + 1];
+    labels[1] = new char [size2 + 1];
+    labels[2] = new char [size3 + 1];
+    labels[3] = new char [size4 + 1];
+
+    strcpy_s(labels[0], size1+1, ch1);
+    strcpy_s(labels[1], size2+1, ch2);
+    strcpy_s(labels[2], size3+1, ch3);
+    strcpy_s(labels[3], size4+1, ch4);
+
+    SDL_Texture* menu[num];
+    bool selected[num] = { 0, 0 };
+    SDL_Color color[2] = { { 0, 0, 0 },{ 255, 0, 0 } };
+
+    TextObject text_object[num];
+    text_object[0].SetText(labels[0]);
+    text_object[0].setColor(color[0].r, color[0].g, color[0].b);
+    text_object[0].loadFromRenderedText(font, g_screen);
+
+    text_object[1].SetText(labels[1]);
+    text_object[1].setColor(color[0].r, color[0].g, color[0].b);
+    text_object[1].loadFromRenderedText(font, g_screen);
+
+    text_object[2].SetText(labels[2]);
+    text_object[2].setColor(color[0].r, color[0].g, color[0].b);
+    text_object[2].loadFromRenderedText(font, g_screen);
+
+    text_object[3].SetText(labels[3]);
+    text_object[3].setColor(color[0].r, color[0].g, color[0].b);
+    text_object[3].loadFromRenderedText(font, g_screen);
+    
+
+
+    SDL_Rect pos[num];
+    pos[0].x = SCREEN_WIDTH*0.3+120;
+    pos[0].y = SCREEN_HEIGHT*0.8 - 80;
+    pos[1].x = SCREEN_WIDTH*0.3+120;
+    pos[1].y = SCREEN_HEIGHT*0.8 + 40;
+    pos[2].x= SCREEN_WIDTH*0.3+60;
+    pos[2].y=SCREEN_HEIGHT*0.8-250;
+    pos[3].x= SCREEN_WIDTH*0.3+20;
+    pos[3].y=SCREEN_HEIGHT*0.8-200;
+
+    
+    
+
+    BaseObject gBackground;
+    bool ret = gBackground.LoadImg(img_file, g_screen);
+
+    SDL_Event event;
+    while (1)
+    {
+        time = SDL_GetTicks();
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                text_object[0].Free();
+                text_object[0].Free();
+                gBackground.Free();
+                return 1;
+            case SDL_MOUSEMOTION:
+                x = event.motion.x;
+                y = event.motion.y;
+                for (int i = 0; i < num-2; ++i)
+                {
+                    if (x >= pos[i].x && x <= pos[i].x + pos[i].w &&
+                        y >= pos[i].y && y <= pos[i].y + pos[i].h)
+                    {
+                        if (!selected[i])
+                        {
+                            selected[i] = 1;
+                            text_object[i].SetText(labels[i]);
+                            text_object[i].setColor(color[1].r, color[1].g, color[1].b);
+                            text_object[i].loadFromRenderedText(font, g_screen);
+                        }
+                    }
+                    else
+                    {
+                        if (selected[i])
+                        {
+                            selected[i] = 0;
+                            text_object[i].Free();
+                            text_object[i].SetText(labels[i]);
+                            text_object[i].setColor(color[0].r, color[0].g, color[0].b);
+                            text_object[i].loadFromRenderedText(font, g_screen);
+                        }
+                    }
+                }
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                x = event.button.x;
+                y = event.button.y;
+                for (int i = 0; i < num-2; ++i) {
+                    if (x >= pos[i].x && x <= pos[i].x + pos[i].w &&
+                        y >= pos[i].y && y <= pos[i].y + pos[i].h)
+                    {
+                        text_object[0].Free();
+                        text_object[1].Free();
+                        gBackground.Free();
+                        return i;
+                    }
+                }
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                {
+                    text_object[0].Free();
+                    text_object[1].Free();
+                    gBackground.Free();
+                    return 1;
+                }
+            break;
+            }
+        }
+
+        gBackground.Render(g_screen, NULL);
+    
+
+        for (int i = 0; i < num; ++i)
+        {
+            text_object[i].RenderText(g_screen, pos[i].x, pos[i].y);
+            pos[i].w = text_object[i].getWidth();
+            pos[i].h = text_object[i].getHeight();
+        }
+
+
+        SDL_RenderPresent(g_screen);
+       if (1000 / 30 > (SDL_GetTicks() - time))
+            SDL_Delay(1000 / 30 - (SDL_GetTicks() - time));
+    }
+    return 0;
+    
+}
 
 bool SDLCommonFunction::CheckCollision(const SDL_Rect& object1, const SDL_Rect& object2)
 {
@@ -15,6 +174,7 @@ bool SDLCommonFunction::CheckCollision(const SDL_Rect& object1, const SDL_Rect& 
     int top_b = object2.y;
     int bottom_b = object2.y + object2.h;
 
+   
     if (left_a > left_b && left_a < right_b)
     {
         if (top_a > top_b && top_a < bottom_b)
@@ -47,6 +207,7 @@ bool SDLCommonFunction::CheckCollision(const SDL_Rect& object1, const SDL_Rect& 
         }
     }
 
+   
     if (left_b > left_a && left_b < right_a)
     {
         if (top_b > top_a && top_b < bottom_a)
@@ -79,6 +240,7 @@ bool SDLCommonFunction::CheckCollision(const SDL_Rect& object1, const SDL_Rect& 
         }
     }
 
+   
     if (top_a == top_b && right_a == right_b && bottom_a == bottom_b)
     {
         return true;
--- game_hunter/CommonFunction.h
+++ game_hunter/CommonFunction.h
@@ -10,12 +10,15 @@
 #include <SDL_image.h>
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
+#include "TextObject.h"
+#include "BaseObject.h"
 
 static SDL_Window* g_window = NULL;
 static SDL_Renderer* g_screen = NULL;
 static SDL_Event g_event;
-static Mix_Chunk* g_sound_ex_main=NULL;
-static Mix_Chunk* g_sound_explosion=NULL;
+static Mix_Chunk* gsound1=NULL;
+static Mix_Chunk* gsound2=NULL;
+
 
 const int FRAMES_PER_SECOND = 30;
 const int SCREEN_WIDTH = 1280;
@@ -29,8 +32,8 @@ const int COLOR_KEY_R = 167;
 const int COLOR_KEY_G = 175;
 const int COLOR_KEY_B = 180;
 
-static char g_name_main_right[]               = {"img//player_right.png"};
-static char g_name_main_left[]              = {"img//player_left.png"};
+static char g_name_main_right[]       = {"img//player_right.png"};
+static char g_name_main_left[]        = {"img//player_left.png"};
 
 #define TILE_SIZE 64
 #define BLANK_TILE 0
@@ -39,7 +42,7 @@ static char g_name_main_left[]              = {"img//player_left.png"};
 #define MAX_MAP_Y 10
 
 #define STATE_MONEY 1
-
+#define level 1
 
 typedef struct Input
 {
@@ -55,15 +58,21 @@ typedef struct Map
 {
     int start_x_;
     int start_y_;
-    int max_x_;
+    int max_x_;  
     int max_y_;   
-    int tile[MAX_MAP_Y][MAX_MAP_X];
+    int tile[MAX_MAP_Y][MAX_MAP_X]; 
     char* file_name_;
 } Map;
 
 namespace SDLCommonFunction
 {
     bool CheckCollision(const SDL_Rect& object1, const SDL_Rect& object2);
+    int ShowMenu(SDL_Renderer* g_screen, TTF_Font* font, 
+                     const std::string& menu1, 
+                   const std::string& menu2,
+                   const std::string& menu3,
+                   const std::string& menu4,
+                    const std::string& img_name);
 }
 
 #endif
